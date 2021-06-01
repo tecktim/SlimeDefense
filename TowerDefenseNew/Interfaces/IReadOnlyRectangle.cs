309213b0c62b8ce94893a78fdfb479e92c1611cc
Tim Friedrich <tifri99@web.de>
2021-06-01 12:24:28 +0200

    Änderungen an der Projektstruktur vorgenommen

---
+++ TowerDefenseNew/Interfaces/IReadOnlyRectangle.cs
@@ -0,0 +1,12 @@
+﻿namespace TowerDefenseNew.Interfaces
+{
+	public interface IReadOnlyRectangle
+	{
+		float MaxX { get; }
+		float MaxY { get; }
+		float MinX { get; }
+		float MinY { get; }
+		float SizeX { get; }
+		float SizeY { get; }
+	}
+}
