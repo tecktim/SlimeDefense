3ea182afc271f90de286eb4ae956c55cd2d34fca
GitMaxWolf <mw-183049@hs-weingarten.de>
2021-06-23 13:02:07 +0200

    add kippe

--- TowerDefenseNew/View.cs
+++ TowerDefenseNew/View.cs
@@ -26,7 +26,7 @@ namespace TowerDefenseNew
             texParticle = TextureLoader.LoadFromResource(content + "water_splash.png");
             texExplosion = TextureLoader.LoadFromResource(content + "smokin.png");
             texFont = TextureLoader.LoadFromResource(content + "sonic_asalga.png");
-            tileSet = TextureLoader.LoadFromResource(content + "TileSet_CG_5x12.png");
+            tileSet = TextureLoader.LoadFromResource(content + "TileSet_CG_5x12_kippe.png");
         }
 
         internal Camera GameCamera { get; } = new Camera(17f, new Vector2(29f, 15f));
